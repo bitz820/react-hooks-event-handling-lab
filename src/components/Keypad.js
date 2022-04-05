bd8ae0c5ece2ff8eeb5ae9004796edbbbd870b7f
Mark <mark.n.bacon.jr@gmail.com>
2022-04-05 18:14:45 -0400

    commit tree 87c711879f1f7c690d2578a9ac2b9fecbc292018

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
-// Code Keypad Component Here
+import React from 'react'
+// import { useState } from 'react/cjs/react.production.min';
+
+const Keypad = () => {
+    // const [password, setPassword] = useState("Password")
+
+
+    function handleChange() {
+        (console.log("Entering password..."))
+    }
+
+    return (
+        <input
+            type="password"
+            onChange={handleChange}
+        />
+    );
+}
+
+export default Keypad;
